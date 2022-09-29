d3b26f8cb5783eca0b264da8e91690a57232b1c5
Alessandro Saggese <asaggese@gmail.com>
2022-09-29 22:22:53 +0200

    update

--- calculator.php
+++ calculator.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
    <head>
-      <meta charset="utf-8">
-      <title></title>
+      <meta charset="UTF-8">
+      <title>My Calculator</title>
    </head>
    <body>
-      <!-- <form action="functions.php" method="get"> -->
       <form>
          <input type="text" name="num01" placeholder="Number 1">
          <input type="text" name="num02" placeholder="Number 2">
          <select name="operator">
-            <label>Choose oparation!</label>
-            <option value="">None</option>
-            <option value="">Add</option>
-            <option value="">Subtract</option>
-            <option value="">Multiply</option>
-            <option value="">Divide</option>
+            <option value="None">None</option>
+            <option value="Add">Add</option>
+            <option value="Subtract">Subtract</option>
+            <option value="Multiply">Multiply</option>
+            <option value="Divide">Divide</option>
          </select>
          <br>
          <button type="submit" name="submit" value="submit">Calculate!</button>
       </form>
+
       <p>The answer is:</p>
+      
       <?php
          if (isset($_GET['submit'])) {
-            $result1 = $_GET['num1'];
-            $result = $_GET['num2'];
+            $result1 = $_GET['num01'];
+            $result2 = $_GET['num02'];
             $operator = $_GET['operator'];
             switch ($operator) {
                case "None": 
-                  echo "You need to select a method!";
+                  echo "You need to select an operation!";
                break;
                case "Add": 
                   echo $result1 + $result2;
