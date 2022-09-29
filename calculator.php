b9859b3bdba2c2a287fdad37920e96c19a307aec
Alessandro Saggese <asaggese@gmail.com>
2022-09-29 22:22:23 +0200

    update

--- calculator.php
+++ calculator.php
@@ -1,71 +1,50 @@
- <!DOCTYPE html>
-<!-- code by webdevtrick ( https://webdevtrick.com) -->
-<html>
-	<head>
-		<title>Simple Calculator In PHP | Webdevtrick.com</title>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		 
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	</head>
-	<body>
-		
-		<div class="container" style="margin-top: 50px">
-		
-			<?php
-			
-				// If the submit button has been pressed
-				if(isset($_POST['submit']))
-				{
-					// Check number values
-					if(is_numeric($_POST['number1']) && is_numeric($_POST['number2']))
-					{
-						// Calculate total
-						if($_POST['operation'] == 'plus')
-						{
-							$total = $_POST['number1'] + $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'minus')
-						{
-							$total = $_POST['number1'] - $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'multiply')
-						{
-							$total = $_POST['number1'] * $_POST['number2'];	
-						}
-						if($_POST['operation'] == 'divided by')
-						{
-							$total = $_POST['number1'] / $_POST['number2'];	
-						}
-						
-						// Print total to the browser
-						echo "<h1>{$_POST['number1']} {$_POST['operation']} {$_POST['number2']} equals {$total}</h1>";
-					
-					} else {
-						
-						// Print error message to the browser
-						echo 'Numeric values are required';
-					
-					}
-				}
-			// end PHP. Code by webdevtrick.com
-			?>
-		    
-		    <!-- Calculator form by webdevtrick.com -->
-		    <form method="post" action="calculator.php">
-		        <input name="number1" type="text" class="form-control" style="width: 150px; display: inline" />
-		        <select name="operation">
-		        	<option value="plus">Plus</option>
-		            <option value="minus">Minus</option>
-		            <option value="multiply">Multiply</option>
-		            <option value="divided by">Devide</option>
-		        </select>
-		        <input name="number2" type="text" class="form-control" style="width: 150px; display: inline" />
-		        <input name="submit" type="submit" value="Calculate" class="btn btn-primary" />
-		    </form>
-	    
-		</div>
-	
-	</body>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+   <head>
+      <meta charset="utf-8">
+      <title></title>
+   </head>
+   <body>
+      <!-- <form action="functions.php" method="get"> -->
+      <form>
+         <input type="text" name="num01" placeholder="Number 1">
+         <input type="text" name="num02" placeholder="Number 2">
+         <select name="operator">
+            <label>Choose oparation!</label>
+            <option value="">None</option>
+            <option value="">Add</option>
+            <option value="">Subtract</option>
+            <option value="">Multiply</option>
+            <option value="">Divide</option>
+         </select>
+         <br>
+         <button type="submit" name="submit" value="submit">Calculate!</button>
+      </form>
+      <p>The answer is:</p>
+      <?php
+         if (isset($_GET['submit'])) {
+            $result1 = $_GET['num1'];
+            $result = $_GET['num2'];
+            $operator = $_GET['operator'];
+            switch ($operator) {
+               case "None": 
+                  echo "You need to select a method!";
+               break;
+               case "Add": 
+                  echo $result1 + $result2;
+               break;
+               case "Subtract": 
+                  echo $result1 - $result2;
+               break;
+               case "Multiply": 
+                  echo $result1 * $result2;
+               break;
+               case "Divide": 
+                  echo $result1 / $result2;
+               break;
+            }
+         }
+      ?>
+
+   </body>
 </html>
